264277ccaec2287e06303f83c2f5d6514b25c722
Eduardo Klein <eduardoklein@MacBook-Air-de-Eduardo.local>
2025-08-13 16:42:53 -0300

    docs: updated readme

--- src/challenges.js
+++ src/challenges.js
@@ -21,7 +21,7 @@ function concatName(arrayDeStrings) {
 // Desafio 4 - Crie a função footballPoints
 
 function footballPoints(wins, ties) {
-  return (wins * 3 + ties);
+  return wins * 3 + ties;
 }
 
 // Desafio 5 - Crie a função highestCount
@@ -30,12 +30,12 @@ function highestCount(arrayNumeros) {
   let maiorNumero = arrayNumeros[0];
   let contador = 0;
 
-  for (index = 0; index <= arrayNumeros.length; index += 1) {
+  for (let index = 0; index < arrayNumeros.length; index += 1) {
     if (arrayNumeros[index] > maiorNumero) {
       maiorNumero = arrayNumeros[index];
     }
   }
-  for (index = 0; index <= arrayNumeros.length; index += 1) {
+  for (let index = 0; index < arrayNumeros.length; index += 1) {
     if (arrayNumeros[index] === maiorNumero) {
       contador += 1;
     }
@@ -45,33 +45,39 @@ function highestCount(arrayNumeros) {
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
 
-function calcTriangleArea(base, height){
-  return (base*height)/2;
+function calcTriangleArea(base, height) {
+  return (base * height) / 2;
 }
 
-function calcRectangleArea(base, height){
-  return base*height;
+function calcRectangleArea(base, height) {
+  return base * height;
 }
 
-function calcAllAreas(base, height, form){
-  if (form === 'triângulo'){
-    return `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}`;
-  }else if (form === 'retângulo'){
-    return `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}`;
-  }else {
+function calcAllAreas(base, height, form) {
+  if (form === 'triângulo') {
+    return `O valor da área do triângulo é de: ${calcTriangleArea(
+      base,
+      height
+    )}`;
+  } else if (form === 'retângulo') {
+    return `O valor da área do retângulo é de: ${calcRectangleArea(
+      base,
+      height
+    )}`;
+  } else {
     return 'Não foi possível fazer o cálculo, insira uma forma geométrica válida';
   }
 }
 
 // Desafio 7 - Crie a função catAndMouse
 
-function catAndMouse(mouse, cat1, cat2){
+function catAndMouse(mouse, cat1, cat2) {
   if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     return 'cat1';
-  } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)){
+  } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)) {
     return 'cat2';
   } else {
-    return 'os gatos trombam e o rato foge'
+    return 'os gatos trombam e o rato foge';
   }
 }
 
@@ -79,15 +85,15 @@ function catAndMouse(mouse, cat1, cat2){
 
 // Desafio 8 - Crie a função fizzBuzz
 
-function fizzBuzz(numeros){
+function fizzBuzz(numeros) {
   const arrayFizzBuzz = [];
-  for (let index = 0; index < numeros.length; ++index){
-    if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0){
+  for (let index = 0; index < numeros.length; ++index) {
+    if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0) {
       arrayFizzBuzz[index] = 'fizzBuzz';
-    }else {
-      if (numeros[index] % 3 === 0){
+    } else {
+      if (numeros[index] % 3 === 0) {
         arrayFizzBuzz[index] = 'fizz';
-      } else if (numeros[index] % 5 === 0){
+      } else if (numeros[index] % 5 === 0) {
         arrayFizzBuzz[index] = 'buzz';
       } else {
         arrayFizzBuzz[index] = 'bug!';
@@ -99,12 +105,22 @@ function fizzBuzz(numeros){
 
 // Desafio 9 - Crie a função encode e a função decode
 
-function encode(fraseParaCodificar){
-  return fraseParaCodificar.replaceAll('a', '1').replaceAll('e', '2').replaceAll('i', '3').replaceAll('o', '4').replaceAll('u', '5');
+function encode(fraseParaCodificar) {
+  return fraseParaCodificar
+    .replaceAll('a', '1')
+    .replaceAll('e', '2')
+    .replaceAll('i', '3')
+    .replaceAll('o', '4')
+    .replaceAll('u', '5');
 }
 
-function decode(fraseParaDecodificar){
-  return fraseParaDecodificar.replaceAll('1', 'a').replaceAll('2', 'e').replaceAll('3', 'i').replaceAll('4', 'o').replaceAll('5', 'u');
+function decode(fraseParaDecodificar) {
+  return fraseParaDecodificar
+    .replaceAll('1', 'a')
+    .replaceAll('2', 'e')
+    .replaceAll('3', 'i')
+    .replaceAll('4', 'o')
+    .replaceAll('5', 'u');
 }
 
 // https://stackoverflow.com/questions/16576983/replace-multiple-characters-in-one-replace-call
@@ -112,27 +128,30 @@ function decode(fraseParaDecodificar){
 
 // Desafio 10 - Crie a função techList
 
-function techList(arrayTech, stringTech){
+function techList(arrayTech, stringTech) {
   const arrayDeObjetos = [];
   for (let index = 0; index < arrayTech.length; index += 1) {
-    arrayDeObjetos.push({name: stringTech, tech: arrayTech.sort()[index]})
+    arrayDeObjetos.push({ name: stringTech, tech: arrayTech.sort()[index] });
   }
   return arrayDeObjetos;
 }
 
 // Não modifique essas linhas
 module.exports = {
-  calcTriangleArea: typeof calcTriangleArea === 'function' ? calcTriangleArea : (() => {}),
-  calcRectangleArea: typeof calcRectangleArea === 'function' ? calcRectangleArea : (() => {}),
-  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : (() => {}),
-  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : (() => {}),
-  compareTrue: typeof compareTrue === 'function' ? compareTrue : (() => {}),
-  concatName: typeof concatName === 'function' ? concatName : (() => {}),
-  decode: typeof decode === 'function' ? decode : (() => {}),
-  encode: typeof encode === 'function' ? encode : (() => {}),
-  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : (() => {}),
-  footballPoints: typeof footballPoints === 'function' ? footballPoints : (() => {}),
-  highestCount: typeof highestCount === 'function' ? highestCount : (() => {}),
-  splitSentence: typeof splitSentence === 'function' ? splitSentence : (() => {}),
-  techList: typeof techList === 'function' ? techList : (() => {}),
+  calcTriangleArea:
+    typeof calcTriangleArea === 'function' ? calcTriangleArea : () => {},
+  calcRectangleArea:
+    typeof calcRectangleArea === 'function' ? calcRectangleArea : () => {},
+  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : () => {},
+  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : () => {},
+  compareTrue: typeof compareTrue === 'function' ? compareTrue : () => {},
+  concatName: typeof concatName === 'function' ? concatName : () => {},
+  decode: typeof decode === 'function' ? decode : () => {},
+  encode: typeof encode === 'function' ? encode : () => {},
+  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : () => {},
+  footballPoints:
+    typeof footballPoints === 'function' ? footballPoints : () => {},
+  highestCount: typeof highestCount === 'function' ? highestCount : () => {},
+  splitSentence: typeof splitSentence === 'function' ? splitSentence : () => {},
+  techList: typeof techList === 'function' ? techList : () => {},
 };
